acb2100a421b75e7301c45bac7c25ebff6e2de1e
Hyunseo Ha <hyunseo.ha@outlook.com>
2024-02-29 08:10:36 +0100

    alias.zsh is added.

---
+++ .alias.zsh
@@ -0,0 +1,35 @@
+alias .="cd"alias .="cd"
+alias ..="cd .."
+
+# directory moving
+alias hoffice="cd /home/hyunseo/Nextcloud/work_hyunseo"
+alias aur="cd /home/hyunseo/Documents/git_repo/arch_aur"
+alias git_hub="cd ~/Documents/git_repo"
+alias memo="cd /home/hyunseo/Nextcloud/personal_hyunseo/memo"
+alias test_folder="cd /home/hyunseo/Nextcloud/2020-08-31-BMDV-5GNPR_Data/Oeffentlichkeitsarbeit/20230703_LEO_WissPaper/power_measure/iperf3_bidirection/iperf3_bidirection_test_script/cp_config_trial_1"
+
+
+# sudo removal
+alias pacman="sudo pacman"
+
+# Ranger config
+alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
+
+# pavuaudio problem removal
+alias pa_removal="rm ~/.config/pulse/*"
+
+# openconnect-sso
+# export QTWEBENGINE_DISABLE_SANDBOX=1
+# export OPENSSL_CONF=~/.my_ssl.conf
+
+# anyconnect running
+alias anyconnect="/opt/cisco/anyconnect/bin/vpnui"
+
+# nextcloud app
+alias nextcloud="~/Nextcloud2/nextcloud_app/Nextcloud-3.10.1-x86_64.AppImage"
+
+# messung_shortcut
+alias messen="cd /home/hyunseo/Nextcloud2/2020-08-31-BMDV-5GNPR_Data/Oeffentlichkeitsarbeit/20230703_LEO_WissPaper/power_consumption_agains_gains/uplink_test"
+
+# Korean INPUT alias
+alias sway='env XDG_CURRENT_DESKTOP=sway GTK_IM_MODULE=kime QT_IM_MODULE=kime XMODIFIERS=@im=kime sway'
